0ea9f6904b8ccc921890b8c0f725fd305b935bd0
Valorie Portem <valorieportem@gmail.com>
2024-09-10 13:23:07 +0800

    initial commit

--- number.js
+++ number.js
@@ -3,8 +3,24 @@ console.log("Addition")
 const firstFavNumber = 21;
 const secondFavNumber = 16;
 const sum = firstFavNumber + secondFavNumber;
-console.log(sum);
+console.log("21 + 16 =", sum);
 
+console.log("Subtraction")
+const difference = firstFavNumber - secondFavNumber;
+console.log("21 - 16 =", difference);
+ 
+console.log("Multiplication")
+const product = firstFavNumber * secondFavNumber;
+console.log("21 * 16 =", product);
+ 
+console.log("Division")
+const quotient = firstFavNumber / secondFavNumber;
+console.log("21 / 16 =", quotient);
 
+console.log("Modulus")
+const mod = firstFavNumber % secondFavNumber;
+console.log("21 % 16 =", mod);
 
-
+console.log("Exponentiation")
+const power = firstFavNumber ** secondFavNumber;
+console.log("21 % 16 =", power);
